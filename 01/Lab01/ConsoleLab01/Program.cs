030660c4c0e1cb4dd4dcb5ff3f97edde928402a0
Sergey <sergey@example.com>
2024-02-13 08:38:29 +0300

    Лабораторная 1.1в Специалист

---
+++ 01/Lab01/ConsoleLab01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLab01
+{
+    public class ThreadClas
+    { 
+        public string ThreadName { get; set; }
+        public int ThreadNumberFrom { get; set; }
+        public int ThreadNumberTo { get; set; }
+
+        public void ThreadExecute()
+        { 
+            Thread.CurrentThread.Name = ThreadName;
+            for (int i = ThreadNumberFrom; i <= ThreadNumberTo; i++)
+            {
+                  Console.WriteLine($"Поток {Thread.CurrentThread.Name} = {i}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread th1 = new Thread(new ThreadClas { ThreadName = "AA", ThreadNumberFrom = 1, ThreadNumberTo = 15 }.ThreadExecute);
+            Thread th2 = new Thread(new ThreadClas { ThreadName = "BB", ThreadNumberFrom = 1, ThreadNumberTo = 20 }.ThreadExecute);
+
+            th1.Start();
+            th2.Start();    
+        }
+    }
+}
