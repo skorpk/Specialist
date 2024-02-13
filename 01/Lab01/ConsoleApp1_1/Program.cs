71daa1037493d2406693e753b950e4ba8d4927be
Sergey <sergey@example.com>
2024-02-13 21:54:57 +0300

    Лабороторная 1_3

---
+++ 01/Lab01/ConsoleApp1_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int n = 0;
+            object syncObj = new object () ;  
+
+            Thread tr1 = new Thread(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    lock (syncObj) 
+                    {
+                        n++;
+                        Console.WriteLine($"Значение = {n.ToString()} в потоке - {Thread.CurrentThread.Name}");
+                    }
+                }
+            })
+            { Name = "A" };
+
+                Thread tr2 = new Thread(() =>
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        lock (syncObj)
+                        {
+                            n++;
+                            Console.WriteLine($"Значение = {n.ToString()} в потоке - {Thread.CurrentThread.Name}");
+                        }
+                    }
+                })
+                { Name = "B" };
+
+            tr1.Start();
+            tr2.Start();
+            Console.WriteLine("-----------------");
+
+        }
+    }
+}
---
+++ 01/Lab01/ConsoleLab1_3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleLab1_3
+{
+    class Sync 
+    {
+        //public double x = 1;
+        public double X { get; set; } = 1;
+
+        public bool Phase { get; set; } = true;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sync sync = new Sync();
+            
+
+            Thread tr1 = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    lock (sync)
+                    {
+                        while (!sync.Phase)
+                            Monitor.Wait(sync);
+
+                        sync.X = Math.Cos(sync.X);
+
+                        Console.Write($"Значение COS = {sync.X.ToString()}");
+
+                        sync.Phase = !sync.Phase;
+                        Monitor.Pulse(sync);
+                    }
+                }
+            })
+            { Name = "A" };
+
+            Thread tr2 = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    lock (sync)
+                    {
+                        while (sync.Phase)
+                            Monitor.Wait(sync);
+
+                        sync.X = Math.Acos(sync.X);
+
+                        Console.WriteLine($" Значение ArCOS = {sync.X.ToString()}");
+                        sync.Phase = !sync.Phase;
+                        Monitor.Pulse(sync);
+                    }
+                }
+            })
+            { Name = "B" };
+
+            tr1.Start();
+            tr2.Start();
+        }
+    }
+}
